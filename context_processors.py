256130a9318df20aeb0422263723571e43367d82
wrwrwr <git@wr.waw.pl>
2013-03-28 17:28:19 +0100

    Coded a simple context processor that adds current site info to template context.

---
+++ context_processors.py
@@ -0,0 +1,9 @@
+from django.contrib.sites.models import get_current_site
+
+
+def site(request):
+    current_site = get_current_site(request)
+    return {
+        'SITE_DOMAIN': current_site.domain,
+        'SITE_NAME': current_site.name
+    }
