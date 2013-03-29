b6b5fe99247755c8a827508307b07c1944162322
wrwrwr <git@wr.waw.pl>
2013-03-29 19:11:50 +0100

    Site context: Strip ending slash from BASE_URL, so it may be used with static tag and media URL.

--- context_processors.py
+++ context_processors.py
@@ -4,7 +4,7 @@ from django.contrib.sites.models import get_current_site
 def site(request):
     current_site = get_current_site(request)
     return {
-        'BASE_URL': request.build_absolute_uri('/'),
+        'BASE_URL': request.build_absolute_uri('/').rstrip('/'),
         'SITE_DOMAIN': current_site.domain,
         'SITE_NAME': current_site.name
     }
