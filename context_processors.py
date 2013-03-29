51867f9c4aaf48ccb7d67d3b8cd06b93e82d8534
wrwrwr <git@wr.waw.pl>
2013-03-29 15:46:45 +0100

    Add a base URL (scheme and host) for the special cases one must use an absolute URI.

--- context_processors.py
+++ context_processors.py
@@ -4,6 +4,7 @@ from django.contrib.sites.models import get_current_site
 def site(request):
     current_site = get_current_site(request)
     return {
+        'BASE_URL': request.build_absolute_uri('/'),
         'SITE_DOMAIN': current_site.domain,
         'SITE_NAME': current_site.name
     }
