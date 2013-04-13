9f486d40fca490eec60f7871d8214c0c4b4052d3
wrwrwr <git@wr.waw.pl>
2013-04-13 17:28:10 +0200

    Separated turning site properties to context constants to a function that can be used without request (e.g. from a signal handler).

--- utils/context_processors.py
+++ utils/context_processors.py
@@ -1,11 +1,15 @@
 from django.contrib.sites.models import get_current_site
 
 
-def site(request):
-    # TODO: Consider using Mezzanine's current_site_id.
-    current_site = get_current_site(request)
+def site_context(site):
     return {
-        'BASE_URL': request.build_absolute_uri('/').rstrip('/'),
-        'SITE_DOMAIN': current_site.domain,
-        'SITE_NAME': current_site.name
+        'SITE_DOMAIN': site.domain,
+        'SITE_NAME': site.name
     }
+
+
+def site(request):
+    current_site = get_current_site(request)
+    context = site_context(current_site)
+    context['BASE_URL'] = request.build_absolute_uri('/').rstrip('/')
+    return context
