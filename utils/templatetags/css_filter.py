4b17db663158513bb17c15c7cad916af034ada06
wrwrwr <git@wr.waw.pl>
2015-05-27 13:02:37 +0200

    Added rgb[a] and hsl[a] colors support in the CSS filter.

--- utils/templatetags/css_filter.py
+++ utils/templatetags/css_filter.py
@@ -3,13 +3,19 @@ import re
 from django import template
 from django.template.defaultfilters import stringfilter
 
-
+ALPHA = '(?:1|(?:0(?:\.\d{1,3})?))'
+PERCENT = '(?:\d{1,3}%)'
 COLOR_HEX_3 = r'#[a-fA-F0-9]{3}'
 COLOR_HEX_6 = r'#[a-fA-F0-9]{6}'
+COLOR_RGB = r'rgb\(\d{1,3},\s?\d{1,3},\s?\d{1,3}\)'
+COLOR_RGBA = r'rgba\(\d{{1,3}},\s?\d{{1,3}},\s?\d{{1,3}},\s?{}\)'.format(ALPHA)
+COLOR_HSL = r'hsl\(\d{1,3},\s?\d{1,3}%,\s?\d{1,3}%\)'
+COLOR_HSLA = r'hsla\(\d{{1,3}},\s?{0},\s?{0},\s?{1}\)'.format(PERCENT, ALPHA)
 COLOR_NAME_BASE = r'aqua|black|blue|fuchsia|gray|grey|green|lime|maroon|' \
                   r'navy|olive|purple|red|silver|teal|white|yellow'
-COLOR = re.compile(r'{}|{}|{}'.format(
-    COLOR_HEX_3, COLOR_HEX_6, COLOR_NAME_BASE))
+COLOR = re.compile(r'{}|{}|{}|{}|{}|{}|{}'.format(
+    COLOR_HEX_3, COLOR_HEX_6, COLOR_RGB, COLOR_RGBA, COLOR_HSL, COLOR_HSLA,
+    COLOR_NAME_BASE))
 
 
 register = template.Library()
